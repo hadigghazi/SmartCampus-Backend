7053866f9b038ca597071b99661614ec64804e4e
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 21:46:21 +0300

    Adding the update contacts migration

--- database/migrations/2024_08_17_183917_create_contacts_table.php
+++ database/migrations/2024_08_17_183917_create_contacts_table.php
@@ -4,24 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateContactsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('relationship', 50);
+            $table->string('phone_number', 20);
+            $table->string('email', 100);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('contacts');
     }
-};
+}
