8830408e099335760b48df8addb404a75e67758f
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 22:11:29 +0300

    Adding the admins migration

---
+++ database/migrations/2024_08_17_191043_create_admins_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('admin_type', ['Super Admin', 'Admin']);
+            $table->foreignId('department_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
