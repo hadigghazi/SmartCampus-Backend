f545ddccc1e810ffe4ab9e4b7b0280e9a688c2c1
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 13:41:13 +0300

    Adding the major model

--- app/Models/Major.php
+++ app/Models/Major.php
@@ -1,11 +1,18 @@
 <?php
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Major extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'description', 'faculty_id'];
+
+    public function faculty()
+    {
+        return $this->belongsTo(Faculty::class);
+    }
 }
