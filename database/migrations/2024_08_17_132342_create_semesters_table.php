992b7de0ede7ffc6d4afc64b019ca1d6ce5e5036
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 16:24:44 +0300

    Adding the semesters migration

---
+++ database/migrations/2024_08_17_132342_create_semesters_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSemestersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('semesters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->boolean('is_current')->default(false);
+            $table->timestamps();
+            $table->softDeletes(); 
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('semesters');
+    }
+}
