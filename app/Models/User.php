74409530038fb05e7266e2466fa4b3bd34fcd4de
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 17:19:55 +0300

    Adding the user migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'first_name', 'middle_name', 'last_name', 'mother_full_name', 'email', 
         'password', 'phone_number', 'role', 'status', 'date_of_birth', 
@@ -17,13 +24,39 @@ class User extends Authenticatable implements JWTSubject
         'marital_status', 'profile_picture'
     ];
 
-    protected $hidden = ['password'];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Get the identifier that will be stored in the JWT subject claim.
+     *
+     * @return mixed
+     */
     public function getJWTIdentifier()
     {
         return $this->getKey();
     }
 
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
     public function getJWTCustomClaims()
     {
         return [];
