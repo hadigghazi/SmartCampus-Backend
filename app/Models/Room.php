0390e9f0d52fcf7fa3051ed1fe5c4385494ae399
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 14:59:38 +0300

    Adding the room model

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Room extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'number',
+        'block_id',
+        'capacity',
+        'description'
+    ];
+
+    public function block()
+    {
+        return $this->belongsTo(Block::class);
+    }
+
+    public function exams()
+    {
+        return $this->hasMany(Exam::class);
+    }
+
+    public function courseInstructors()
+    {
+        return $this->hasMany(CourseInstructor::class);
+    }
 }
