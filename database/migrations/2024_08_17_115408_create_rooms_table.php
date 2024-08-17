77523882cd1fe5c32dda4a031f4ec52f98a525c1
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 14:54:52 +0300

    Adding the rooms migration

---
+++ database/migrations/2024_08_17_115408_create_rooms_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('number', 20);
+            $table->foreignId('block_id')->constrained()->onDelete('cascade');
+            $table->integer('capacity');
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
