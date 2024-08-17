c2b8e0e70301eea3d267d92a3b469500663c79b0
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 10:05:13 +0300

    Adding the user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,36 +2,106 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    /**
+     * Display a listing of users.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
         $users = User::all();
-        return response()->json($users);
+        return UserResource::collection($users);
     }
 
+    /**
+     * Display the specified user.
+     *
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function show(User $user)
     {
-        return response()->json($user);
+        return new UserResource($user);
     }
 
-    public function store(Request $request)
+    /**
+     * Store a newly created user in storage.
+     *
+     * @param \App\Http\Requests\StoreUserRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(StoreUserRequest $request)
     {
-        $user = User::create($request->all());
-        return response()->json($user, 201);
+        $validatedData = $request->validated();
+        $validatedData['password'] = bcrypt($validatedData['password']);
+
+        $user = User::create($validatedData);
+        return new UserResource($user);
     }
 
-    public function update(Request $request, User $user)
+    /**
+     * Update the specified user in storage.
+     *
+     * @param \App\Http\Requests\UpdateUserRequest $request
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(UpdateUserRequest $request, User $user)
     {
-        $user->update($request->all());
-        return response()->json($user);
+        $validatedData = $request->validated();
+
+        if (isset($validatedData['password'])) {
+            $validatedData['password'] = bcrypt($validatedData['password']);
+        }
+
+        $user->update($validatedData);
+        return new UserResource($user);
     }
 
+    /**
+     * Remove the specified user from storage (soft delete).
+     *
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function destroy(User $user)
     {
+        if ($user->trashed()) {
+            return response()->json([
+                'message' => 'User is already deleted'
+            ], 404);
+        }
+
         $user->delete();
-        return response()->json(null, 204);
+        return response()->json([
+            'message' => 'User successfully deleted',
+            'user' => new UserResource($user)
+        ], 200);
+    }
+
+    /**
+     * Restore a soft-deleted user.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function restore($id)
+    {
+        $user = User::withTrashed()->findOrFail($id);
+        if (!$user->trashed()) {
+            return response()->json([
+                'message' => 'User is not deleted'
+            ], 404);
+        }
+
+        $user->restore();
+        return new UserResource($user);
     }
 }
