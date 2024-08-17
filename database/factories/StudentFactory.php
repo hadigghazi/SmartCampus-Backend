de189e2a276674e43adad513d865b394a029c4f4
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 21:29:28 +0300

    Adding students factory

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -2,22 +2,31 @@
 
 namespace Database\Factories;
 
+use App\Models\Student;
+use App\Models\User;
+use App\Models\Semester;
+use App\Models\Contact;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
- */
 class StudentFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Student::class;
+
+    public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'government_id' => $this->faker->bothify('??#####'),
+            'civil_status_number' => $this->faker->bothify('??#####'),
+            'passport_number' => $this->faker->bothify('??#####'),
+            'visa_status' => $this->faker->word(),
+            'native_language' => $this->faker->word(),
+            'secondary_language' => $this->faker->word(),
+            'current_semester_id' => Semester::factory(),
+            'additional_info' => $this->faker->paragraph(),
+            'transportation' => $this->faker->boolean(),
+            'dorm_residency' => $this->faker->boolean(),
+            'emergency_contact_id' => Contact::factory(),
         ];
     }
 }
