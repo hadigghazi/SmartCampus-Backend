e33d8e15401b072ff4af1df9d2dff914073c9395
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 15:24:03 +0300

    Adding the course controller

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,64 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Models\Course;
-use App\Http\Requests\StoreCourseRequest;
-use App\Http\Requests\UpdateCourseRequest;
+use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $courses = Course::withTrashed()->get();
+        return response()->json($courses);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(StoreCourseRequest $request)
     {
-        //
+        $course = Course::create($request->validated());
+        return response()->json($course, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Course $course)
     {
-        //
+        return response()->json($course);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Course $course)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateCourseRequest $request, Course $course)
     {
-        //
+        $course->update($request->validated());
+        return response()->json($course);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Course $course)
     {
-        //
+        $course->delete();
+        return response()->json(null, 204);
+    }
+
+    public function restore($id)
+    {
+        $course = Course::withTrashed()->findOrFail($id);
+        $course->restore();
+
+        return response()->json($course);
+    }
+
+    public function forceDelete($id)
+    {
+        $course = Course::withTrashed()->findOrFail($id);
+        $course->forceDelete();
+
+        return response()->json(null, 204);
     }
 }
