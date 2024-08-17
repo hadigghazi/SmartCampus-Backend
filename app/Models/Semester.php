fcde755c79af773414cef26ca622927a86588819
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 16:26:23 +0300

    Adding the semester model

--- app/Models/Semester.php
+++ app/Models/Semester.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Semester extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'start_date', 'end_date', 'is_current'];
+
+    public function registrations()
+    {
+        return $this->hasMany(Registration::class);
+    }
+
+    public function courseInstructors()
+    {
+        return $this->hasMany(CourseInstructor::class);
+    }
 }
