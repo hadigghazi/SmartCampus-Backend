c18e2d4376b1984f2bf52fa723056bdac7410c54
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 16:34:08 +0300

    Adding the update semester request

--- app/Http/Requests/UpdateSemesterRequest.php
+++ app/Http/Requests/UpdateSemesterRequest.php
@@ -6,23 +6,18 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateSemesterRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
+    public function authorize()
     {
-        return false;
+        return true; 
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'name' => 'required|string|max:50',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+            'is_current' => 'nullable|boolean',
         ];
     }
 }
