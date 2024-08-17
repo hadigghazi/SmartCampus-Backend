697d85fc84c5090546da261723ebaae05148dfe0
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 22:28:36 +0300

    Adding the instructor update request

--- app/Http/Requests/UpdateInstructorRequest.php
+++ app/Http/Requests/UpdateInstructorRequest.php
@@ -1,28 +1,21 @@
 <?php
-
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateInstructorRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
+    public function authorize()
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'user_id' => 'required|exists:users,id',
+            'department_id' => 'required|exists:departments,id',
+            'specialization' => 'sometimes|required|string|max:100',
         ];
     }
 }
