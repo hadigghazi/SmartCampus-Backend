166e267b4328faafb25456711205dae8df096deb
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 16:27:02 +0300

    Adding the semester factory

--- database/factories/SemesterFactory.php
+++ database/factories/SemesterFactory.php
@@ -2,22 +2,20 @@
 
 namespace Database\Factories;
 
+use App\Models\Semester;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Semester>
- */
 class SemesterFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Semester::class;
+
+    public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word . ' Semester',
+            'start_date' => $this->faker->dateTimeBetween('-1 year', 'now')->format('Y-m-d'),
+            'end_date' => $this->faker->dateTimeBetween('now', '+1 year')->format('Y-m-d'),
+            'is_current' => $this->faker->boolean,
         ];
     }
 }
