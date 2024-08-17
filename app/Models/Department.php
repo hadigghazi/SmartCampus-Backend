26bbea45bcff7c26c2d972ac782acf40bed663ee
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 12:46:21 +0300

    Adding the departements migrations

---
+++ app/Models/Department.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Department extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'description', 'campus_id'];
+
+    public function campus()
+    {
+        return $this->belongsTo(Campus::class);
+    }
+
+    public function instructors()
+    {
+        return $this->hasMany(Instructor::class);
+    }
+
+    public function courses()
+    {
+        return $this->hasMany(Course::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([ CampusSeeder::class,FacultySeeder::class]);
+        $this->call([ CampusSeeder::class,FacultySeeder::class, DepartmentSeeder::class]);
     }
 }
