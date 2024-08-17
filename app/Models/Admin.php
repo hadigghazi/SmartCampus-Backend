7bb1099913b498dd096a483ff5c4306ccb60b3de
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 22:12:23 +0300

    Adding the admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Admin extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'admin_type',
+        'department_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
