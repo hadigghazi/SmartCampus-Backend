93e476c7a7f993113ad830a0aacc7881ed50a34b
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 15:04:03 +0300

    Adding the room controller

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -5,62 +5,54 @@ namespace App\Http\Controllers;
 use App\Models\Room;
 use App\Http\Requests\StoreRoomRequest;
 use App\Http\Requests\UpdateRoomRequest;
+use Illuminate\Http\Request;
 
 class RoomController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $rooms = Room::withTrashed()->get();
+        return response()->json($rooms);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreRoomRequest $request)
     {
-        //
+        $room = Room::create($request->validated());
+
+        return response()->json($room, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Room $room)
     {
-        //
+        return response()->json($room);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Room $room)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateRoomRequest $request, Room $room)
     {
-        //
+        $room->update($request->validated());
+
+        return response()->json($room);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Room $room)
     {
-        //
+        $room->delete();
+        return response()->json(null, 204);
+    }
+
+    public function restore($id)
+    {
+        $room = Room::withTrashed()->findOrFail($id);
+        $room->restore();
+
+        return response()->json($room);
+    }
+
+    public function forceDelete($id)
+    {
+        $room = Room::withTrashed()->findOrFail($id);
+        $room->forceDelete();
+
+        return response()->json(null, 204);
     }
 }
