ec468c9f89a5522f851e7922996e0755046fba7e
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 22:32:54 +0300

    Adding the instructor factory

--- database/factories/InstructorFactory.php
+++ database/factories/InstructorFactory.php
@@ -1,23 +1,19 @@
 <?php
-
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Instructor;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Instructor>
- */
 class InstructorFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Instructor::class;
+
+    public function definition()
     {
         return [
-            //
+            'user_id' => \App\Models\User::factory(),
+            'department_id' => \App\Models\Department::factory(),
+            'specialization' => $this->faker->word(),
         ];
     }
 }
