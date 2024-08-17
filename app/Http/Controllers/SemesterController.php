8e9daf453cdb264278e47282149ec7b4ccdb50c8
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 16:30:13 +0300

    Adding the index in semester controller

--- app/Http/Controllers/SemesterController.php
+++ app/Http/Controllers/SemesterController.php
@@ -3,64 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Semester;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreSemesterRequest;
 use App\Http\Requests\UpdateSemesterRequest;
 
 class SemesterController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $semesters = Semester::withTrashed()->get();
+        return response()->json($semesters);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreSemesterRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Semester $semester)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Semester $semester)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateSemesterRequest $request, Semester $semester)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Semester $semester)
-    {
-        //
-    }
+   
 }
