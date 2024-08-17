0b971413c96a9712646dc6727bd25425e820fb02
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 21:44:56 +0300

    Adding the update contacts factory

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -3,21 +3,21 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
- */
 class ContactFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = \App\Models\Contact::class;
+
+    public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'relationship' => $this->faker->word,
+            'phone_number' => $this->faker->phoneNumber,
+            'email' => $this->faker->unique()->safeEmail,
         ];
     }
 }
