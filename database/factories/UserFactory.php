24f44fb16464276aec41df7e7445ed49a83134b5
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-17 09:54:17 +0300

    Adding the user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,44 +1,31 @@
 <?php
-
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+    protected $model = \App\Models\User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'first_name' => $this->faker->firstName,
+            'middle_name' => $this->faker->optional()->firstName,
+            'last_name' => $this->faker->lastName,
+            'mother_full_name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => bcrypt('password'),
+            'phone_number' => $this->faker->phoneNumber,
+            'role' => $this->faker->randomElement(['Student', 'Admin', 'Instructor']),
+            'status' => $this->faker->randomElement(['Pending', 'Approved', 'Rejected']),
+            'date_of_birth' => $this->faker->date(),
+            'nationality' => $this->faker->country,
+            'second_nationality' => $this->faker->optional()->country,
+            'country_of_birth' => $this->faker->country,
+            'gender' => $this->faker->randomElement(['Male', 'Female']),
+            'marital_status' => $this->faker->randomElement(['Single', 'Married', 'Divorced', 'Widowed']),
+            'profile_picture' => $this->faker->imageUrl(),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
