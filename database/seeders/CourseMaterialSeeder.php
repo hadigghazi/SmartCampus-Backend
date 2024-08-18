12e5f7c8e500842ab0063c3034a615b8c3d0e4a7
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:05:23 +0300

    Adding seeder for CourseMaterial

--- database/seeders/CourseMaterialSeeder.php
+++ database/seeders/CourseMaterialSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\CourseMaterial;
 
 class CourseMaterialSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        CourseMaterial::factory()->count(10)->create();
     }
 }
