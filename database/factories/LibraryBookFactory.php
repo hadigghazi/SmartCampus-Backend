d130e5a13f303a092508be15b49df815f784fca8
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:12:41 +0300

    Adding factory for LibraryBook

--- database/factories/LibraryBookFactory.php
+++ database/factories/LibraryBookFactory.php
@@ -2,22 +2,23 @@
 
 namespace Database\Factories;
 
+use App\Models\LibraryBook;
+use App\Models\Campus; // Reference to Campus factory
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\LibraryBook>
- */
 class LibraryBookFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = LibraryBook::class;
+
+    public function definition()
     {
         return [
-            //
+            'title' => $this->faker->sentence(),
+            'author' => $this->faker->name(),
+            'isbn' => $this->faker->isbn13(),
+            'copies' => $this->faker->numberBetween(1, 100),
+            'publication_year' => $this->faker->year(),
+            'campus_id' => Campus::factory(), // Reference the Campus factory
         ];
     }
 }
