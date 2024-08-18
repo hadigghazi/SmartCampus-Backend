ea60974bf825d2d5d31201781b241e07f300d7a0
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:20:02 +0300

    Adding migration for book_borrows

--- database/migrations/2024_08_18_161954_create_book_borrows_table.php
+++ database/migrations/2024_08_18_161954_create_book_borrows_table.php
@@ -4,24 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateBookBorrowTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('book_borrows', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('student_id')->constrained('students')->onDelete('cascade');
+            $table->foreignId('book_id')->constrained('library_books')->onDelete('cascade');
+            $table->date('due_date');
+            $table->date('return_date')->nullable();
+            $table->enum('status', ['Borrowed', 'Returned', 'Overdue'])->default('Borrowed');
+            $table->text('notes')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('book_borrows');
     }
-};
+}
