e36b9a98c455068a5bd4956aa90f9314ce3aef50
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:05:20 +0300

    Adding content to CourseMaterial model

--- app/Models/CourseMaterial.php
+++ app/Models/CourseMaterial.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CourseMaterial extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'course_id',
+        'title',
+        'description',
+        'file_path',
+        'uploaded_by',
+    ];
+
+    protected $dates = ['deleted_at'];
 }
