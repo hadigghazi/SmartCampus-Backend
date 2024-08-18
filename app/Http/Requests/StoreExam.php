7e51aeffead6f6c45f2bd656d9c0456409a1ad64
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 13:16:45 +0300

    Adding store request for Exam

--- app/Http/Requests/StoreExam.php
+++ app/Http/Requests/StoreExam.php
@@ -6,23 +6,16 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreExam extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'course_id' => 'required|integer|exists:courses,id',
+            'instructor_id' => 'required|integer|exists:instructors,id',
+            'date' => 'required|date',
+            'time' => 'required|date_format:H:i:s',
+            'duration' => 'required|integer',
+            'campus_id' => 'required|integer|exists:campuses,id',
+            'room_id' => 'required|integer|exists:rooms,id',
         ];
     }
 }
