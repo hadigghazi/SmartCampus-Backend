9ddf20ec164818bccf6884e4f386d15bdf7deafe
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 13:44:35 +0300

    Adding migration for assignments

--- database/migrations/2024_08_18_104431_create_assignments_table.php
+++ database/migrations/2024_08_18_104431_create_assignments_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateassignmentsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('assignments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('course_id');
+            $table->string('title', 100);
+            $table->text('description');
+            $table->date('due_date');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('assignments');
     }
-};
+}
