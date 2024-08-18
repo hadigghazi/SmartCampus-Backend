aa3542a3d45d53582fe77c1b02dfb75ba5c3fbce
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 14:52:50 +0300

    Adding content to FinancialAidScholarship model with relationships

--- app/Models/FinancialAidScholarship.php
+++ app/Models/FinancialAidScholarship.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FinancialAidScholarship extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'student_id',
+        'type',
+        'amount_usd',
+        'amount_lbp',
+        'description',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    // Relationships
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
 }
