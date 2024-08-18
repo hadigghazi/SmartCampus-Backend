b530160decc1d7ec25adc809ed507e33cd37d51e
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 15:50:45 +0300

    Adding migration for bus_registrations

--- database/migrations/2024_08_18_125039_create_bus_registrations_table.php
+++ database/migrations/2024_08_18_125039_create_bus_registrations_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateBusRegistrationTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('bus_registrations', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('student_id')->constrained()->onDelete('cascade');
+            $table->foreignId('bus_route_id')->constrained()->onDelete('cascade');
+            $table->date('registration_date');
+            $table->enum('status', ['Pending', 'Confirmed', 'Canceled'])->default('Pending');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('bus_registrations');
     }
-};
+}
