a55e2a6473fbe2cd741d96e9c4c5e20b48236d16
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 17:31:31 +0300

    Adding content to Announcement model

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Announcement extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'published_date',
+        'author_id',
+        'visibility',
+        'category',
+    ];
+
+    protected $dates = ['published_date', 'deleted_at'];
 }
