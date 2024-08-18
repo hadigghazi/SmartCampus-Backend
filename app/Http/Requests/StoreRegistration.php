0a6a701ce149b42669c2a07a0ab5f55d6639a22e
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 12:50:05 +0300

    Added store request for Registration

--- app/Http/Requests/StoreRegistration.php
+++ app/Http/Requests/StoreRegistration.php
@@ -6,23 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreRegistration extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'student_id' => 'required|integer',
+            'course_id' => 'required|integer',
+            'instructor_id' => 'required|integer',
+            'semester_id' => 'required|integer',
+            'status' => 'required|in:Registered,Completed,Failed',
         ];
     }
 }
