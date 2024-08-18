39e9f31e0ba54ca9636972f1fadb981f9f76f72e
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:28:25 +0300

    Adding store request for AIInstructorInteraction

--- app/Http/Requests/StoreAIInstructorInteraction.php
+++ app/Http/Requests/StoreAIInstructorInteraction.php
@@ -6,23 +6,12 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreAIInstructorInteraction extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'user_id' => 'required|exists:users,id',
+            'question' => 'required|string',
+            'answer' => 'required|string',
         ];
     }
 }
