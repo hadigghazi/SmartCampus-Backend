bac7ca031123ded014e5fe93a84b0ef5748d7419
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 10:47:33 +0300

    Adding dorm factory

--- database/factories/DormFactory.php
+++ database/factories/DormFactory.php
@@ -2,22 +2,23 @@
 
 namespace Database\Factories;
 
+use App\Models\Dorm;
+use App\Models\Campus;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Dorm>
- */
 class DormFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Dorm::class;
+
+    public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word(),
+            'description' => $this->faker->text(),
+            'capacity' => $this->faker->numberBetween(50, 500),
+            'available_rooms' => $this->faker->numberBetween(1, 100),
+            'campus_id' => Campus::inRandomOrder()->first()->id,
+            'address' => $this->faker->address(),
         ];
     }
 }
