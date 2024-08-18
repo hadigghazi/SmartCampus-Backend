fc318597f8ba091abfb5cc077a31df971f95256d
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 14:51:09 +0300

    Adding content to Fee model with relationships

--- app/Models/Fee.php
+++ app/Models/Fee.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fee extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'description',
+        'amount_usd',
+        'amount_lbp',
+        'exchange_rate',
+        'faculty_id',
+        'credit_price_usd',
+        'credit_price_lbp',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    // Relationships
+    public function faculty()
+    {
+        return $this->belongsTo(Faculty::class);
+    }
 }
