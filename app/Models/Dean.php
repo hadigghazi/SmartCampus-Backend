09def4cdf7b0ba4902064a8ae1b3d746692e83a6
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 17:38:49 +0300

    Adding content to Dean model

--- app/Models/Dean.php
+++ app/Models/Dean.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Dean extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'faculty_id',
+        'campus_id',
+        'role_description',
+    ];
+
+    protected $dates = ['deleted_at'];
 }
