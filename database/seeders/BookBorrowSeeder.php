1c5f95e54ab1e914e0a82c0676852da8e1a7e810
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:20:04 +0300

    Adding seeder for BookBorrow

--- database/seeders/BookBorrowSeeder.php
+++ database/seeders/BookBorrowSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\BookBorrow;
 
 class BookBorrowSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        BookBorrow::factory()->count(10)->create();
     }
 }
