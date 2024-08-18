99ea36601db493b88af4c733fa4c56abc1ef6ee8
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 14:28:28 +0300

    Adding content to Payment model with relationships

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Payment extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'student_id',
+        'description',
+        'amount_usd',
+        'amount_lbp',
+        'exchange_rate',
+        'payment_date',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    // Relationships
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
 }
