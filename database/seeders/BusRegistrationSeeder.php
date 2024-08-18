5d1e0c550a6f65157edf9c77c2e53930561b7c4b
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 15:50:46 +0300

    Adding seeder for BusRegistration

--- database/seeders/BusRegistrationSeeder.php
+++ database/seeders/BusRegistrationSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\BusRegistration;
 
 class BusRegistrationSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        BusRegistration::factory()->count(10)->create();
     }
 }
