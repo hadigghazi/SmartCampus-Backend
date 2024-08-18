0729946054f9f6162c0da2c6305fe3b682e9f385
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 09:07:45 +0300

    Adding facultycampus index

--- app/Http/Controllers/FacultyCampusController.php
+++ app/Http/Controllers/FacultyCampusController.php
@@ -3,64 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\FacultyCampus;
-use App\Http\Requests\StoreFacultyCampusRequest;
-use App\Http\Requests\UpdateFacultyCampusRequest;
+use Illuminate\Http\Request;
 
 class FacultyCampusController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $facultiesCampuses = FacultyCampus::withTrashed()->get();
+        return response()->json($facultiesCampuses);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreFacultyCampusRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(FacultyCampus $facultyCampus)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(FacultyCampus $facultyCampus)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateFacultyCampusRequest $request, FacultyCampus $facultyCampus)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(FacultyCampus $facultyCampus)
-    {
-        //
-    }
 }
