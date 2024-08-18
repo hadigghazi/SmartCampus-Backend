2a6f4f9db730c8d3a7821bbc00e9278773719c70
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:27:01 +0300

    Adding seeder for CourseDropRequest

--- database/seeders/CourseDropRequestSeeder.php
+++ database/seeders/CourseDropRequestSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\CourseDropRequest;
 
 class CourseDropRequestSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        CourseDropRequest::factory()->count(10)->create();
     }
 }
