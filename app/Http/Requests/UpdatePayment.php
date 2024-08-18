64f7157b97bad0b80a971ae2db7725bfa1ab1546
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 14:28:31 +0300

    Adding update request for Payment

--- app/Http/Requests/UpdatePayment.php
+++ app/Http/Requests/UpdatePayment.php
@@ -6,23 +6,15 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdatePayment extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'student_id' => 'sometimes|integer|exists:students,id',
+            'description' => 'sometimes|string',
+            'amount_usd' => 'sometimes|numeric|min:0',
+            'amount_lbp' => 'sometimes|numeric|min:0',
+            'exchange_rate' => 'sometimes|numeric|min:0',
+            'payment_date' => 'sometimes|date',
         ];
     }
 }
