c224d2de7b6619324d1942e4b3153758405c55ca
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 17:31:33 +0300

    Adding migration for announcements

--- database/migrations/2024_08_18_143125_create_announcements_table.php
+++ database/migrations/2024_08_18_143125_create_announcements_table.php
@@ -4,24 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateAnnouncementTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('announcements', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('content');
+            $table->date('published_date');
+            $table->foreignId('author_id')->constrained('users')->onDelete('cascade');
+            $table->string('visibility', 50);
+            $table->string('category', 50);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('announcements');
     }
-};
+}
