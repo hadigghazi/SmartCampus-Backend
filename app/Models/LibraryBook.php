7824170b7744c17af4bbd0eefc0625110e1de75f
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:12:39 +0300

    Adding content to LibraryBook model

--- app/Models/LibraryBook.php
+++ app/Models/LibraryBook.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LibraryBook extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'author',
+        'isbn',
+        'copies',
+        'publication_year',
+        'campus_id',
+    ];
+
+    protected $dates = ['deleted_at'];
 }
