8af535f444620610c948bdd95d1c65f9caa6478b
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 13:40:03 +0300

    Adding store request for Grade

--- app/Http/Requests/StoreGrade.php
+++ app/Http/Requests/StoreGrade.php
@@ -6,23 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreGrade extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'registration_id' => 'required|integer|exists:registrations,id',
+            'grade' => 'required|numeric|min:0|max:100',
+            'letter_grade' => 'required|in:A,B,C,D,F',
+            'gpa' => 'required|numeric|min:0|max:4',
         ];
     }
 }
