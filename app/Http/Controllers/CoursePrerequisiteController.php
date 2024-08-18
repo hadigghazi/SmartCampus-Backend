8cc2d61ba587fff7fda0bb90985c689d5dc6b453
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 10:02:59 +0300

    Adding course prerequisite index

--- app/Http/Controllers/CoursePrerequisiteController.php
+++ app/Http/Controllers/CoursePrerequisiteController.php
@@ -8,59 +8,10 @@ use App\Http\Requests\UpdateCoursePrerequisiteRequest;
 
 class CoursePrerequisiteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $coursePrerequisites = CoursePrerequisite::withTrashed()->get();
+        return response()->json($coursePrerequisites);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreCoursePrerequisiteRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(CoursePrerequisite $coursePrerequisite)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(CoursePrerequisite $coursePrerequisite)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateCoursePrerequisiteRequest $request, CoursePrerequisite $coursePrerequisite)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(CoursePrerequisite $coursePrerequisite)
-    {
-        //
-    }
 }
