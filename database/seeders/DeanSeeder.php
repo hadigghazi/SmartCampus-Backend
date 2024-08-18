546c836e477a3b7e18585287b9732cfdbb9dfe7c
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 17:38:52 +0300

    Adding seeder for Dean

--- database/seeders/DeanSeeder.php
+++ database/seeders/DeanSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Dean;
 
 class DeanSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        Dean::factory()->count(10)->create();
     }
 }
