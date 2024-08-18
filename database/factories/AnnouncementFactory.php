df1fe206702e842f2517f4bc85e44c4418d293c7
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 17:31:34 +0300

    Adding factory for Announcement

--- database/factories/AnnouncementFactory.php
+++ database/factories/AnnouncementFactory.php
@@ -2,22 +2,22 @@
 
 namespace Database\Factories;
 
+use App\Models\Announcement;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Announcement>
- */
 class AnnouncementFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Announcement::class;
+
+    public function definition()
     {
         return [
-            //
+            'title' => $this->faker->sentence(),
+            'content' => $this->faker->paragraph(),
+            'published_date' => $this->faker->date(),
+            'author_id' => $this->faker->numberBetween(1, 100),
+            'visibility' => $this->faker->word(),
+            'category' => $this->faker->word(),
         ];
     }
 }
