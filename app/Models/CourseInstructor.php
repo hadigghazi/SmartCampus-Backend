ff7b76f50d6c4687372843e09bc59a0d9e865435
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 10:27:06 +0300

    Adding course instructors model

--- app/Models/CourseInstructor.php
+++ app/Models/CourseInstructor.php
@@ -2,10 +2,49 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CourseInstructor extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'course_instructors';
+
+    protected $fillable = [
+        'course_id',
+        'instructor_id',
+        'semester_id',
+        'capacity',
+        'campus_id',
+        'room_id',
+        'schedule',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function instructor()
+    {
+        return $this->belongsTo(Instructor::class);
+    }
+
+    public function semester()
+    {
+        return $this->belongsTo(Semester::class);
+    }
+
+    public function campus()
+    {
+        return $this->belongsTo(Campus::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
