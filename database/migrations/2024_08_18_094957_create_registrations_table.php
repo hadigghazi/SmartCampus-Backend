d0388a5fca19e9d62a18ea8b70508e8d27fe48e4
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 12:50:03 +0300

    Added migration for registrations

--- database/migrations/2024_08_18_094957_create_registrations_table.php
+++ database/migrations/2024_08_18_094957_create_registrations_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateregistrationsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('registrations', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('course_id');
+            $table->unsignedBigInteger('instructor_id');
+            $table->unsignedBigInteger('semester_id');
+            $table->enum('status', ['Registered', 'Completed', 'Failed'])->default('Registered');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('registrations');
     }
-};
+}
