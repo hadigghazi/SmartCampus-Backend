1b1dd663fb37f16696a00d39c0d818d4da5f9bb2
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 11:21:24 +0300

    Adding bus route factory

--- database/factories/BusRouteFactory.php
+++ database/factories/BusRouteFactory.php
@@ -2,22 +2,22 @@
 
 namespace Database\Factories;
 
+use App\Models\BusRoute;
+use App\Models\Campus;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BusRoute>
- */
 class BusRouteFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = BusRoute::class;
+
+    public function definition()
     {
         return [
-            //
+            'route_name' => $this->faker->word,
+            'description' => $this->faker->text,
+            'schedule' => $this->faker->text,
+            'capacity' => $this->faker->numberBetween(10, 100),
+            'campus_id' => Campus::inRandomOrder()->first()->id,
         ];
     }
 }
