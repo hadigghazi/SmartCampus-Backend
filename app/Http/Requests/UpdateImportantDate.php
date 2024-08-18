3cd19b087120f61e8bff90317f97a477a80d62d2
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 15:35:27 +0300

    Adding update request for ImportantDate

--- app/Http/Requests/UpdateImportantDate.php
+++ app/Http/Requests/UpdateImportantDate.php
@@ -6,23 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateImportantDate extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'title' => 'sometimes|string|max:100',
+            'description' => 'sometimes|string',
+            'date' => 'sometimes|date',
+            'end_date' => 'sometimes|date',
+            'type' => 'sometimes|in:Deadline,Event,Holiday,Other',
         ];
     }
 }
