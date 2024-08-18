e5e06e6baa6fcb5cbbca66076c8db6d9c90551d8
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 15:35:27 +0300

    Adding seeder for ImportantDate

--- database/seeders/ImportantDateSeeder.php
+++ database/seeders/ImportantDateSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\ImportantDate;
 
 class ImportantDateSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        ImportantDate::factory()->count(10)->create();
     }
 }
