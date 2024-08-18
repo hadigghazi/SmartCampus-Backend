a29e51669277f5e8b18e1804f90cf86ff1e49085
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:05:23 +0300

    Adding store request for CourseMaterial

--- app/Http/Requests/StoreCourseMaterial.php
+++ app/Http/Requests/StoreCourseMaterial.php
@@ -6,23 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreCourseMaterial extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'course_id' => 'required|exists:courses,id',
+            'title' => 'required|string|max:100',
+            'description' => 'required|string',
+            'file_path' => 'required|string|max:255',
+            'uploaded_by' => 'required|exists:users,id',
         ];
     }
 }
