0c44812db597e11a87c0b1288c656cc2f3227fbe
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 17:25:58 +0300

    Adding content to News model

--- app/Models/News.php
+++ app/Models/News.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class News extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'published_date',
+        'author_id',
+        'category',
+    ];
+
+    protected $dates = ['published_date', 'deleted_at'];
 }
