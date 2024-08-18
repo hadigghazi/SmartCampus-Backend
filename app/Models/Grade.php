08cdc3d0ff51c32415a9ba295d073daceb526c67
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 13:40:01 +0300

    Adding content to Grade model with relationships

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Grade extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'registration_id',
+        'grade',
+        'letter_grade',
+        'gpa',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    // Relationships
+    public function registration()
+    {
+        return $this->belongsTo(Registration::class);
+    }
 }
