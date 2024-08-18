1d7775ad935d129db77c2578797585bc617395fa
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:05:24 +0300

    Adding update request for CourseMaterial

--- app/Http/Requests/UpdateCourseMaterial.php
+++ app/Http/Requests/UpdateCourseMaterial.php
@@ -6,23 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateCourseMaterial extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'course_id' => 'sometimes|exists:courses,id',
+            'title' => 'sometimes|string|max:100',
+            'description' => 'sometimes|string',
+            'file_path' => 'sometimes|string|max:255',
+            'uploaded_by' => 'sometimes|exists:users,id',
         ];
     }
 }
