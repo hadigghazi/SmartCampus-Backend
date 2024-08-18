df4ab2d9ff45363fa73220dc39bce86d32266405
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 13:16:43 +0300

    Adding factory for Exam

--- database/factories/ExamFactory.php
+++ database/factories/ExamFactory.php
@@ -2,22 +2,23 @@
 
 namespace Database\Factories;
 
+use App\Models\Exam;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Exam>
- */
 class ExamFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Exam::class;
+
+    public function definition()
     {
         return [
-            //
+            'course_id' => \App\Models\Course::inRandomOrder()->first()->id,
+            'instructor_id' => \App\Models\Instructor::inRandomOrder()->first()->id,
+            'date' => $this->faker->date(),
+            'time' => $this->faker->time(),
+            'duration' => $this->faker->numberBetween(60, 180),
+            'campus_id' => \App\Models\Campus::inRandomOrder()->first()->id,
+            'room_id' => \App\Models\Room::inRandomOrder()->first()->id,
         ];
     }
 }
