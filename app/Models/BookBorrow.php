c731ff03fe2db56be24a5074086aa4ea5f1ddd2e
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:20:01 +0300

    Adding content to BookBorrow model

--- app/Models/BookBorrow.php
+++ app/Models/BookBorrow.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BookBorrow extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'student_id',
+        'book_id',
+        'due_date',
+        'return_date',
+        'status',
+        'notes',
+    ];
+
+    protected $dates = ['due_date', 'return_date', 'deleted_at'];
 }
