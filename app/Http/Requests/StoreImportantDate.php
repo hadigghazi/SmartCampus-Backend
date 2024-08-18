03a62957d6e89766556a9aff5e02aa11febf1228
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 15:35:27 +0300

    Adding store request for ImportantDate

--- app/Http/Requests/StoreImportantDate.php
+++ app/Http/Requests/StoreImportantDate.php
@@ -6,23 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreImportantDate extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'title' => 'required|string|max:100',
+            'description' => 'nullable|string',
+            'date' => 'required|date',
+            'end_date' => 'nullable|date',
+            'type' => 'required|in:Deadline,Event,Holiday,Other',
         ];
     }
 }
