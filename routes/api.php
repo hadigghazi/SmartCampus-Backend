15d098a90590473894c64d01300f0856735863ef
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 13:40:04 +0300

    Adding routes for Grade to routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -124,3 +124,6 @@ Route::delete('registrations/{id}/force-delete', [RegistrationController::class,
 Route::apiResource('exams', ExamController::class);
 Route::post('exams/{id}/restore', [ExamController::class, 'restore']);
 Route::delete('exams/{id}/force-delete', [ExamController::class, 'forceDelete']);
+Route::apiResource('grades', GradeController::class);
+Route::post('grades/{id}/restore', [GradeController::class, 'restore']);
+Route::delete('grades/{id}/force-delete', [GradeController::class, 'forceDelete']);
