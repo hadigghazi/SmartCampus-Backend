8a1463e02723e56386f8488ae3500421c80e9416
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:26:59 +0300

    Adding migration for course_drop_requests

--- database/migrations/2024_08_18_162650_create_course_drop_requests_table.php
+++ database/migrations/2024_08_18_162650_create_course_drop_requests_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateCourseDropRequestTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('course_drop_requests', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('student_id')->constrained('students')->onDelete('cascade');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->text('reason');
+            $table->enum('status', ['Pending', 'Approved', 'Rejected'])->default('Pending');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('course_drop_requests');
     }
-};
+}
