9040c6e4e9f4834070acc4e6b07e84c289381118
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 13:40:03 +0300

    Adding update request for Grade

--- app/Http/Requests/UpdateGrade.php
+++ app/Http/Requests/UpdateGrade.php
@@ -6,23 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateGrade extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'registration_id' => 'sometimes|integer|exists:registrations,id',
+            'grade' => 'sometimes|numeric|min:0|max:100',
+            'letter_grade' => 'sometimes|in:A,B,C,D,F',
+            'gpa' => 'sometimes|numeric|min:0|max:4',
         ];
     }
 }
