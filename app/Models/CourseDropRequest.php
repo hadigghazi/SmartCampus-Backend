1c5c1d9c75088d8b3f88e52803601d99712d6b30
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:26:58 +0300

    Adding content to CourseDropRequest model

--- app/Models/CourseDropRequest.php
+++ app/Models/CourseDropRequest.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CourseDropRequest extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'student_id',
+        'course_id',
+        'reason',
+        'status',
+    ];
+
+    protected $dates = ['deleted_at'];
 }
