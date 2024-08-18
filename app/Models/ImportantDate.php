7db1173199a3608c977970da94065c5c8189ae43
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 15:35:25 +0300

    Adding content to ImportantDate model

--- app/Models/ImportantDate.php
+++ app/Models/ImportantDate.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ImportantDate extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'date',
+        'end_date',
+        'type',
+    ];
+
+    protected $dates = ['deleted_at'];
 }
