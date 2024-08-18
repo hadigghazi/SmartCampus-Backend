a31b9e3d31de24aabede452179f68a18a1b7eb9a
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 17:25:59 +0300

    Adding factory for News

--- database/factories/NewsFactory.php
+++ database/factories/NewsFactory.php
@@ -2,22 +2,21 @@
 
 namespace Database\Factories;
 
+use App\Models\News;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\News>
- */
 class NewsFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = News::class;
+
+    public function definition()
     {
         return [
-            //
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraph,
+            'published_date' => $this->faker->date(),
+            'author_id' => $this->faker->numberBetween(1, 100),
+            'category' => $this->faker->word,
         ];
     }
 }
