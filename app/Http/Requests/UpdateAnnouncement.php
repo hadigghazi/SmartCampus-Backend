fe32a0ace022d9b20eb202982ac20b07d9c59af4
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 17:31:35 +0300

    Adding update request for Announcement

--- app/Http/Requests/UpdateAnnouncement.php
+++ app/Http/Requests/UpdateAnnouncement.php
@@ -6,23 +6,15 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateAnnouncement extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'title' => 'sometimes|string|max:100',
+            'content' => 'sometimes|string',
+            'published_date' => 'sometimes|date',
+            'author_id' => 'sometimes|exists:users,id',
+            'visibility' => 'sometimes|string|max:50',
+            'category' => 'sometimes|string|max:50',
         ];
     }
 }
