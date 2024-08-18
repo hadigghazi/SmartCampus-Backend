21cae81678d1446caa19ca63bc1bf6d87421e3d2
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 09:03:34 +0300

    Adding facultycampus model

--- app/Models/FacultyCampus.php
+++ app/Models/FacultyCampus.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FacultyCampus extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'faculty_id',
+        'campus_id',
+    ];
+
+    public function faculty()
+    {
+        return $this->belongsTo(Faculty::class);
+    }
+
+    public function campus()
+    {
+        return $this->belongsTo(Campus::class);
+    }
 }
