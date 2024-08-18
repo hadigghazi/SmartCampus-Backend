0b73c7ea7a4a96d18d9757902dda0a3bc290dabe
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 14:52:51 +0300

    Adding migration for financial_aid_scholarships

--- database/migrations/2024_08_18_115246_create_financial_aid_scholarships_table.php
+++ database/migrations/2024_08_18_115246_create_financial_aid_scholarships_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class Createfinancial_aid_scholarshipsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('financial_aid_scholarships', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->string('type', 100);
+            $table->decimal('amount_usd', 10, 2);
+            $table->decimal('amount_lbp', 10, 2);
+            $table->text('description');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('financial_aid_scholarships');
     }
-};
+}
