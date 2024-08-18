10f2c8b61419fba01ef3ba0cb5793d65250fc785
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:05:21 +0300

    Adding migration for course_materials

--- database/migrations/2024_08_18_160514_create_course_materials_table.php
+++ database/migrations/2024_08_18_160514_create_course_materials_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateCourseMaterialTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('course_materials', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('file_path', 255);
+            $table->foreignId('uploaded_by')->constrained('users')->onDelete('cascade');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('course_materials');
     }
-};
+}
