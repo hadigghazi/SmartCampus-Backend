a362caf581ec40fced836b3b78638564b3ceb64a
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 15:50:45 +0300

    Adding content to BusRegistration model

--- app/Models/BusRegistration.php
+++ app/Models/BusRegistration.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BusRegistration extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'student_id',
+        'bus_route_id',
+        'registration_date',
+        'status',
+    ];
+
+    protected $dates = ['deleted_at'];
 }
