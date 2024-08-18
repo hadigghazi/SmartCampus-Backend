af8e1256452cf054b13d4895cc332a155f87b17c
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 15:42:45 +0300

    Adding store request for DormRegistration

--- app/Http/Requests/StoreDormRegistration.php
+++ app/Http/Requests/StoreDormRegistration.php
@@ -6,23 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreDormRegistration extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'student_id' => 'required|exists:students,id',
+            'dorm_room_id' => 'required|exists:dorm_rooms,id',
+            'start_date' => 'required|date',
+            'end_date' => 'nullable|date',
+            'status' => 'required|in:Pending,Confirmed,Canceled',
         ];
     }
 }
