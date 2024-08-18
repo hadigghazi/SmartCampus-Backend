3d253a3b49c41b86d4133cc2154409a4058fe284
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 10:45:24 +0300

    Adding dorm model

--- app/Models/Dorm.php
+++ app/Models/Dorm.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Dorm extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'capacity',
+        'available_rooms',
+        'campus_id',
+        'address',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function campus()
+    {
+        return $this->belongsTo(Campus::class);
+    }
 }
