83a7436f1c3443e1125aa72f4555e85756afb6d0
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 14:51:11 +0300

    Adding seeder for Fee

--- database/seeders/FeeSeeder.php
+++ database/seeders/FeeSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Fee;
 
 class FeeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        Fee::factory()->count(10)->create();
     }
 }
