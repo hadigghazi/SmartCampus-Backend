ef844b9c355c9af83c8f2b46c90e1de65b5bf5db
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:12:42 +0300

    Adding seeder for LibraryBook

--- database/seeders/LibraryBookSeeder.php
+++ database/seeders/LibraryBookSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\LibraryBook;
 
 class LibraryBookSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        LibraryBook::factory()->count(10)->create();
     }
 }
