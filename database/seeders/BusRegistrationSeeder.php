d95cc93ba0a4e4f21f60a7b6438e16514ea5164d
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 15:50:45 +0300

    Generating BusRegistrationController, BusRegistrationFactory, BusRegistrationSeeder, and request classes

---
+++ database/seeders/BusRegistrationSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BusRegistrationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
