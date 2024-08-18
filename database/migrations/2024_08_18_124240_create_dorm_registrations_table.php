095c02b7d01d36e38948360b9787914435ea8e60
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 15:42:44 +0300

    Adding migration for dorm_registrations

--- database/migrations/2024_08_18_124240_create_dorm_registrations_table.php
+++ database/migrations/2024_08_18_124240_create_dorm_registrations_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateDormRegistrationTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('dorm_registrations', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('student_id')->constrained()->onDelete('cascade');
+            $table->foreignId('dorm_room_id')->constrained()->onDelete('cascade');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->enum('status', ['Pending', 'Confirmed', 'Canceled'])->default('Pending');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('dorm_registrations');
     }
-};
+}
