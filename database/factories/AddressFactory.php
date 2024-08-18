1a25f7406d65189d5e10e0412a15557792b8fde9
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 08:18:41 +0300

    Adding the address factory

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -1,23 +1,24 @@
 <?php
-
 namespace Database\Factories;
 
+use App\Models\Address;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Address>
- */
 class AddressFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Address::class;
+
+    public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'country' => $this->faker->country,
+            'area' => $this->faker->streetSuffix,
+            'city' => $this->faker->city,
+            'street' => $this->faker->streetAddress,
+            'building_floor' => $this->faker->secondaryAddress,
+            'special_instruction' => $this->faker->sentence,
         ];
     }
 }
