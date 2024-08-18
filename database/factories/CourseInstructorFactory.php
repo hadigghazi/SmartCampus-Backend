cf3360d96876d2f1293605383fb9705c21eef1e5
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 10:20:36 +0300

    Adding course instructors factory

--- database/factories/CourseInstructorFactory.php
+++ database/factories/CourseInstructorFactory.php
@@ -2,22 +2,28 @@
 
 namespace Database\Factories;
 
+use App\Models\CourseInstructor;
+use App\Models\Course;
+use App\Models\Instructor;
+use App\Models\Semester;
+use App\Models\Campus;
+use App\Models\Room;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CourseInstructor>
- */
 class CourseInstructorFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = CourseInstructor::class;
+
+    public function definition()
     {
         return [
-            //
+            'course_id' => Course::inRandomOrder()->first()->id,
+            'instructor_id' => Instructor::inRandomOrder()->first()->id,
+            'semester_id' => Semester::inRandomOrder()->first()->id,
+            'capacity' => $this->faker->numberBetween(10, 50),
+            'campus_id' => Campus::inRandomOrder()->first()->id,
+            'room_id' => Room::inRandomOrder()->first()->id,
+            'schedule' => $this->faker->word() . ' ' . $this->faker->time(),
         ];
     }
 }
