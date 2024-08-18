f136a9fd800cb875d0944ae4284bba39520abf8c
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 19:12:43 +0300

    Adding update request for LibraryBook

--- app/Http/Requests/UpdateLibraryBook.php
+++ app/Http/Requests/UpdateLibraryBook.php
@@ -6,23 +6,15 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateLibraryBook extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'title' => 'sometimes|string|max:100',
+            'author' => 'sometimes|string|max:100',
+            'isbn' => 'sometimes|string|max:20',
+            'copies' => 'sometimes|integer',
+            'publication_year' => 'sometimes|integer',
+            'campus_id' => 'sometimes|exists:campuses,id',
         ];
     }
 }
