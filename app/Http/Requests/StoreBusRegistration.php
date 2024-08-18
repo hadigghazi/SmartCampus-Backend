ce48dda93900f222959dc82a15582038246d4c1d
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 15:50:47 +0300

    Adding store request for BusRegistration

--- app/Http/Requests/StoreBusRegistration.php
+++ app/Http/Requests/StoreBusRegistration.php
@@ -6,23 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreBusRegistration extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'student_id' => 'required|exists:students,id',
+            'bus_route_id' => 'required|exists:bus_routes,id',
+            'registration_date' => 'required|date',
+            'status' => 'required|in:Pending,Confirmed,Canceled',
         ];
     }
 }
