7358951ecc79db7fdebaa8976b5c19716b65b009
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 14:20:55 +0300

    Adding update request for Submission

--- app/Http/Requests/UpdateSubmission.php
+++ app/Http/Requests/UpdateSubmission.php
@@ -6,23 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateSubmission extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'assignment_id' => 'sometimes|integer|exists:assignments,id',
+            'student_id' => 'sometimes|integer|exists:students,id',
+            'file_path' => 'sometimes|string|max:255',
+            'submission_date' => 'sometimes|date',
+            'grade' => 'sometimes|numeric|min:0|max:100',
         ];
     }
 }
