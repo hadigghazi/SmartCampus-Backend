116557c725be9728edf71ab1fcfb4d57dfadc274
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 09:04:30 +0300

    Adding facultycampus store request

--- app/Http/Requests/StoreFacultyCampusRequest.php
+++ app/Http/Requests/StoreFacultyCampusRequest.php
@@ -6,23 +6,16 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreFacultyCampusRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
+    public function authorize()
     {
-        return false;
+        return true; 
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'faculty_id' => 'required|exists:faculties,id',
+            'campus_id' => 'required|exists:campuses,id',
         ];
     }
 }
