90ccaddc9f49bc675d8b20de4e7174bc38b88284
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 11:19:04 +0300

    Adding bus route model

--- app/Models/BusRoute.php
+++ app/Models/BusRoute.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BusRoute extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'route_name',
+        'description',
+        'schedule',
+        'capacity',
+        'campus_id',
+    ];
+
+    public function campus()
+    {
+        return $this->belongsTo(Campus::class);
+    }
 }
