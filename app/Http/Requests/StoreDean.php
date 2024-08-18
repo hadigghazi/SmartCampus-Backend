53e9e62cc5a2e5d81524f0c74d132045304ce9b8
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 17:38:53 +0300

    Adding store request for Dean

--- app/Http/Requests/StoreDean.php
+++ app/Http/Requests/StoreDean.php
@@ -6,23 +6,12 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreDean extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'faculty_id' => 'required|exists:faculties,id',
+            'campus_id' => 'required|exists:campuses,id',
+            'role_description' => 'required|string',
         ];
     }
 }
