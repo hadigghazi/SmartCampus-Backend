ff742da47f5c41f40ef40635ae529f87db6b0948
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 15:42:43 +0300

    Adding content to DormRegistration model

--- app/Models/DormRegistration.php
+++ app/Models/DormRegistration.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DormRegistration extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'student_id',
+        'dorm_room_id',
+        'start_date',
+        'end_date',
+        'status',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function dormRoom()
+    {
+        return $this->belongsTo(DormRoom::class);
+    }
 }
