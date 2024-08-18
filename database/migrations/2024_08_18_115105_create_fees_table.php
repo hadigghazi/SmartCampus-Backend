b563c5f53553a3d3401c2341974cdd7a70712ecb
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 14:51:09 +0300

    Adding migration for fees

--- database/migrations/2024_08_18_115105_create_fees_table.php
+++ database/migrations/2024_08_18_115105_create_fees_table.php
@@ -4,24 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatefeesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('fees', function (Blueprint $table) {
             $table->id();
+            $table->text('description');
+            $table->decimal('amount_usd', 10, 2);
+            $table->decimal('amount_lbp', 10, 2);
+            $table->decimal('exchange_rate', 10, 4);
+            $table->unsignedBigInteger('faculty_id');
+            $table->decimal('credit_price_usd', 10, 2);
+            $table->decimal('credit_price_lbp', 10, 2);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('fees');
     }
-};
+}
