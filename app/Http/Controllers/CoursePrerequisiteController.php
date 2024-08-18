d644af2daa7f58abe8666ac11a673166a7ca4307
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-18 09:57:48 +0300

    Adding course prerequisites migration

---
+++ app/Http/Controllers/CoursePrerequisiteController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CoursePrerequisite;
+use App\Http\Requests\StoreCoursePrerequisiteRequest;
+use App\Http\Requests\UpdateCoursePrerequisiteRequest;
+
+class CoursePrerequisiteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCoursePrerequisiteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CoursePrerequisite $coursePrerequisite)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CoursePrerequisite $coursePrerequisite)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCoursePrerequisiteRequest $request, CoursePrerequisite $coursePrerequisite)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CoursePrerequisite $coursePrerequisite)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_08_18_065651_create_course_prerequisites_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursePrerequisitesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('course_prerequisites', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id');
+            $table->unsignedBigInteger('prerequisite_course_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Foreign keys
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->foreign('prerequisite_course_id')->references('id')->on('courses')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('course_prerequisites');
+    }
+}
