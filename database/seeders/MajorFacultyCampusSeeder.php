28058cc7ecf69b9dbe911f703041f82983addbbf
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-21 09:50:45 +0300

    Adding seeder for MajorFacultyCampus

--- database/seeders/MajorFacultyCampusSeeder.php
+++ database/seeders/MajorFacultyCampusSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\MajorFacultyCampus;
 
 class MajorFacultyCampusSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        MajorFacultyCampus::factory()->count(10)->create();
     }
 }
