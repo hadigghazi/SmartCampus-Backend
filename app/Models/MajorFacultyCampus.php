4649bfc8bb97f553cc7da3374dcb670b9882f3fa
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-21 09:50:44 +0300

    Adding content to MajorFacultyCampus model

--- app/Models/MajorFacultyCampus.php
+++ app/Models/MajorFacultyCampus.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MajorFacultyCampus extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'major_id',
+        'faculty_campus_id',
+    ];
+
+    protected $dates = ['deleted_at'];
 }
