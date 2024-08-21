45b0f01962d07d3d51ed688d2020098903090d8d
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-21 09:50:44 +0300

    Adding migration for majors_faculties_campuses

---
+++ database/migrations/_create_majors_faculties_campuses_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMajorFacultyCampusTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('majors_faculties_campuses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('major_id')->constrained('majors')->onDelete('cascade');
+            $table->foreignId('faculty_campus_id')->constrained('faculties_campuses')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('majors_faculties_campuses');
+    }
+}
