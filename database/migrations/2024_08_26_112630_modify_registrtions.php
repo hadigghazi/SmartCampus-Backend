5d5913dfc7a7e2f95ad24050be253df5ad243ec0
Hadi Ghazi <hadigghazi@gmail.com>
2024-08-26 14:34:39 +0300

    Fixing registrations table

---
+++ database/migrations/2024_08_26_112630_modify_registrtions.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('registrations', function (Blueprint $table) {
+            $table->dropColumn('course_id');
+            $table->dropColumn('instructor_id');
+         
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('registrations', function (Blueprint $table) {
+
+            $table->dropColumn('course_instructor_id');
+
+        });
+    }
+};
