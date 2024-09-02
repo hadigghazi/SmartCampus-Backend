9b5da2c92ec3b898930f526370c90c77d352d23d
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-02 10:33:11 +0300

    Adding financial aid and scholarship model

--- app/Models/FinancialAidScholarship.php
+++ app/Models/FinancialAidScholarship.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FinancialAidScholarship extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'student_id',
+        'amount',
+        'type',
+        'effective_date',
+    ];
+
+    protected $dates = ['deleted_at'];
 }
