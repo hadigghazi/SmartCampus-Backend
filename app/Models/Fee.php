c76a097e40803af917683d50109a2efa5a574c2e
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-02 10:29:43 +0300

    Adding payment fee model

--- app/Models/Fee.php
+++ app/Models/Fee.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fee extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'student_id',
+        'course_id',
+        'description',
+        'amount_usd',
+        'amount_lbp',
+    ];
+
+    protected $dates = ['deleted_at'];
 }
