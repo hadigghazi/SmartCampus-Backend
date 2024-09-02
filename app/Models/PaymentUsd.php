90bca934dd17d95968e15a40e29e8d41ab573416
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-02 10:30:56 +0300

    Adding payment usd model

--- app/Models/PaymentUsd.php
+++ app/Models/PaymentUsd.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PaymentUsd extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'student_id',
+        'description',
+        'amount_paid',
+        'payment_date',
+    ];
+
+    protected $dates = ['deleted_at'];
 }
