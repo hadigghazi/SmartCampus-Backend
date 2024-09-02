370e90f7cc2135ca59ebd52f7e23dffe99cb0c51
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-02 13:39:04 +0300

    Creating payments table

---
+++ database/migrations/2024_09_02_103442_create_payment.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->text('description');
+            $table->decimal('amount_paid', 10, 2);
+            $table->date('payment_date');
+            $table->enum('currency', ['USD', 'LBP']);
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment');
+    }
+};
