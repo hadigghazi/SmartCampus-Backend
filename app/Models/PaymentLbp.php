78bed983bfbd6ba12540ff733645ad54eb205d5d
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-02 10:31:55 +0300

    Adding payment lbp model

--- app/Models/PaymentLbp.php
+++ app/Models/PaymentLbp.php
@@ -1,11 +1,20 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PaymentLbp extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'student_id',
+        'description',
+        'amount_paid',
+        'payment_date',
+    ];
+
+    protected $dates = ['deleted_at'];
 }
