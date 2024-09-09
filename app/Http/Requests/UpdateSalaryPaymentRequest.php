b218adeffe05f9aadf3fdc5f1be14a5fc0bc129d
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-09 10:52:33 +0300

    Creating the salary payment update request

--- app/Http/Requests/UpdateSalaryPaymentRequest.php
+++ app/Http/Requests/UpdateSalaryPaymentRequest.php
@@ -6,23 +6,18 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateSalaryPaymentRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
+    public function authorize()
     {
-        return false;
+        return true; 
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'amount' => 'nullable|numeric|min:0',
+            'payment_date' => 'nullable|date',
+            'recipient_id' => 'nullable|exists:users,id',
+            'recipient_type' => 'nullable|in:Instructor,Admin',
         ];
     }
 }
